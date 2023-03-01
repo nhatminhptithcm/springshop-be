7afb031c47d642d476da5947e450e0fbf909dcef
nhatminhptithcm <nhatminhptithcm@gmail.com>
2023-03-02 03:20:39 +0700

    Init project and add category functionalities

---
+++ src/main/java/edu/poly/springshop/exception/CategoryExceptionResponse.java
@@ -0,0 +1,13 @@
+package edu.poly.springshop.exception;
+
+import lombok.Data;
+
+@Data
+public class CategoryExceptionResponse {
+
+    private String message;
+
+    public CategoryExceptionResponse(String message) {
+        this.message = message;
+    }
+}
